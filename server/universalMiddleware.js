253e81d0cc5c47cc21eee3383457ba313824b7a6
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2018-07-09 12:32:24 +1000

    feat: Introduce a dedicated module for setting up the universal renderer

---
+++ server/universalMiddleware.js
@@ -0,0 +1,44 @@
+import path from 'path';
+import {createMemoryHistory} from "history";
+
+import createUniversalRenderer from "./reactools-express/createUniversalRenderer";
+import App from "../src/App";
+import createStore from "../src/internals/store";
+
+import setupRedux from "./reactools-express/setupRedux";
+import setupHelmet from "./reactools-express/setupHelmet";
+import setupReactRouter from "./reactools-express/setupReactRouter";
+import setupLoadable from "./reactools-express/setupLoadable";
+import setupStyledComponents from './reactools-express/setupStyledComponents';
+
+
+// get the html file created with the create-react-app build
+const filePath = path.resolve(__dirname, '..', 'build', 'index.html');
+const manifest = require(`../${"build"}/asset-manifest.json`); // import the manifest generated with the create-react-app build
+
+// Setup the redux store and initialize the redux adapter
+const history = createMemoryHistory();
+const store = createStore({}, history);
+setupRedux.init(store);
+
+// Setup the react-loadable adapter
+setupLoadable.init(manifest);
+
+export default createUniversalRenderer({
+  App,
+  index: filePath,
+  adapters: {
+    router: setupReactRouter,
+    redux: setupRedux,
+    helmet: setupHelmet,
+    styled: setupStyledComponents,
+    loadable: setupLoadable,
+  },
+  features: [
+    'router',
+    'redux',
+    'helmet',
+    'styled',
+    'loadable',
+  ]
+});
