a0bc5ae55d1dff23dc461c1bbbe40864f72a2eb9
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2018-07-09 12:31:46 +1000

    feat: Implement SSR adapter for styled-components

---
+++ server/reactools-express/setupStyledComponents.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {ServerStyleSheet, StyleSheetManager} from 'styled-components';
+
+class SetupStyledComponents {
+
+  constructor() {
+    this.sheet = new ServerStyleSheet();
+  }
+
+  replacer() {
+    return [
+      '<style class="_styled_hydrate_"></style>', `${this.sheet.getStyleTags()}`
+    ]
+  }
+
+  render(children) {
+    return <StyleSheetManager sheet={this.sheet.instance}>
+      {children}
+    </StyleSheetManager>
+  }
+
+}
+
+export default new SetupStyledComponents();
