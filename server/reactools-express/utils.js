4692904cba3b84afb35bfd4c581f92ebaebd4946
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2018-07-09 12:33:10 +1000

    refactor: Moveout extractChunks helper to the dedicated adapter

--- server/reactools-express/utils.js
+++ server/reactools-express/utils.js
@@ -1,4 +1,9 @@
-// function to extract js assets from the manifest
-export const extractAssets = (assets, chunks) => Object.keys(assets)
-  .filter(asset => chunks.indexOf(asset.replace('.js', '')) > -1)
-  .map(k => assets[k]);
+
+export const replaceHTML = (html, replacer) => html.replace(replacer[0], replacer[1]);
+
+export const processHTML = (htmlData, features, adapters) => features.reduce((processedHtmlData, feature) => {
+  if (adapters[feature]) {
+    return replaceHTML(processedHtmlData, adapters[feature].replacer());
+  }
+  return processedHtmlData;
+}, htmlData);
