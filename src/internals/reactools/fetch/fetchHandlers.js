9907e48674b16fec4736c7e4a7a4af749c018118
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2018-07-11 17:35:32 +1000

    refactor: Merge fetchHandler and fetch metadata handlers to a single module

--- src/internals/reactools/fetch/fetchHandlers.js
+++ src/internals/reactools/fetch/fetchHandlers.js
@@ -9,7 +9,7 @@ import {HTTP_METHODS, HTTP_CODES} from '../constants';
  * @param {object} payload - Query params or post body(Will be switched based on HTTP method)
  * @returns {Promise} - response data from backend
  */
-export async function fetchHandler(entry, payload) {
+export async function doFetch(entry, payload) {
 
   let {url, options} = entry;
   // Note - metadata will not be validated here expecting that the metadata file is perfect and predictable
@@ -70,4 +70,58 @@ export async function fetchHandler(entry, payload) {
   }
 
   throw err;
+}
+
+
+/**
+ * Create a generic action from provided metadata corresponding to the dispatched fetch action
+ *
+ * @param {string} type - action type to be dispatched
+ * @param {any} payload - response data payload from the fetch
+ * @param {object} error - any errors occurred during the fetch
+ * @param {object} args - default data provided in metadata to be shipped with the action
+ * @return {object} redux action
+ */
+function createReplyAction(type, payload, error, args = {}) {
+  return {
+    type,
+    payload,
+    error,
+    ...args
+  };
+}
+
+/**
+ * Analyze the reply action and create actions from metadata accordingly
+ *
+ * @param {object} reply - success or failure reply from the fetch handler
+ * @return {Array} redux actions array
+ */
+export function getReplyActions(reply) {
+
+  let replyActions = [];
+
+  if (!reply.replyAction) {
+    return replyActions;
+  }
+
+  // Treat non array replies as an array with one element (to make the preceding code more meaningful)
+  let replies = Array.isArray(reply.replyAction) ? reply.replyAction : [reply.replyAction];
+
+  replies.forEach(replyAction => {
+
+    // If the reply action is a string, we will follow default behavior
+    if (typeof replyAction === "string") {
+      replyActions.push(createReplyAction(replyAction, reply.data, reply.error));
+    }
+
+    // If the reply action is an object, and it has a field: "type" we merge it with the dispatched action
+    if (typeof replyAction === "object" && replyAction.type) {
+      let metadataAction = {...replyAction};
+      delete metadataAction.type;
+      replyActions.push(createReplyAction(replyAction.type, reply.data, reply.error, metadataAction));
+    }
+  });
+
+  return replyActions;
 }
