2c01aa9ca056652342062300bd18bf95367de225
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2018-07-11 17:34:32 +1000

    refactor: Moveout preprocess logic and metadata analysis logic out of the fetch saga

--- src/internals/reactools/fetch/fetchSaga.js
+++ src/internals/reactools/fetch/fetchSaga.js
@@ -1,90 +1,7 @@
 import {all, call, put, takeEvery, takeLatest, takeLeading} from 'redux-saga/effects';
 import {REDUX_ACTIONS} from '../constants';
-import {fetchHandler} from './sagaHandlers';
-
-
-let preProcessors = [];
-
-/**
- * Pre process the corresponding fetch metadata entry with previously registered pre processors
- * @param entry - fetch metadata entry
- */
-function preProcess(entry) {
-  return preProcessors.reduce((processedEntry, {flag, handler}) => {
-    if (entry[flag]) {
-      return {
-        ...processedEntry,
-        options: handler(processedEntry.options) || processedEntry.options
-      };
-    }
-    return processedEntry;
-  }, entry);
-}
-
-/**
- * Register a middleware to modify the metadata entry before making the fetch call
- * @param flag - a flag to check if we need to call the handler or not
- * @param handler - pre processor to do exactly what it means
- */
-export function registerPreProcessor(flag, handler) {
-  preProcessors.push({
-    flag,
-    handler
-  })
-}
-
-/**
- * Create a generic action from provided metadata corresponding to the dispatched fetch action
- *
- * @param {string} type - action type to be dispatched
- * @param {any} payload - response data payload from the fetch
- * @param {object} error - any errors occurred during the fetch
- * @param {object} args - default data provided in metadata to be shipped with the action
- * @return {object} redux action
- */
-function createReplyAction(type, payload, error, args = {}) {
-  return {
-    type,
-    payload,
-    error,
-    ...args
-  };
-}
-
-/**
- * Analyze the reply action and create actions from metadata accordingly
- *
- * @param {object} reply - success or failure reply from the fetch handler
- * @return {Array} redux actions array
- */
-function getReplyActions(reply) {
-
-  let replyActions = [];
-
-  if (!reply.replyAction) {
-    return replyActions;
-  }
-
-  // Treat non array replies as an array with one element (to make the preceding code more meaningful)
-  let replies = Array.isArray(reply.replyAction) ? reply.replyAction : [reply.replyAction];
-
-  replies.forEach(replyAction => {
-
-    // If the reply action is a string, we will follow default behavior
-    if (typeof replyAction === "string") {
-      replyActions.push(createReplyAction(replyAction, reply.data, reply.error));
-    }
-
-    // If the reply action is an object, and it has a field: "type" we merge it with the dispatched action
-    if (typeof replyAction === "object" && replyAction.type) {
-      let metadataAction = {...replyAction};
-      delete metadataAction.type;
-      replyActions.push(createReplyAction(replyAction.type, reply.data, reply.error, metadataAction));
-    }
-  });
-
-  return replyActions;
-}
+import {doFetch, getReplyActions} from './fetchHandlers';
+import {preProcess} from './preProcess';
 
 
 /**
@@ -102,8 +19,12 @@ function* fetchActionHandler(metadata, action) {
 
   // Handle the fetch call
   try {
-    reply = yield call(fetchHandler, preProcess(entry), action.payload);
+    // Dispatch INIT action to signal the supporting reducers/middleware if any
+    yield put({...action, type: REDUX_ACTIONS.FETCH_INIT});
 
+    reply = yield call(doFetch, preProcess(entry), action.payload);
+
+    // If the code reaches this point, rather than going to the catch, that is an indication that the fetch is a success
     // Fire action to be used by the fetch statuses reducer
     yield put({...action, type: REDUX_ACTIONS.FETCH_SUCCESS});
 
