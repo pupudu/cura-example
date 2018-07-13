7ca6e3bb50881c050db1952f6ffc36f1aab63380
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2018-07-13 15:12:11 +1000

    feat: Update redirect middleware to handle actions which appends a path rather than doing a complete redirect

--- src/internals/reactools/redirectMiddleware.js
+++ src/internals/reactools/redirectMiddleware.js
@@ -7,16 +7,30 @@
 
 import {push} from 'connected-react-router';
 
-export const createRedirectMiddleware = (actionType) => {
+export const createRedirectMiddleware = (redirectAction, redirectAppendAction) => {
   return store => next => action => {
-    if (action.type === actionType) {
-      if (action.url) {
-        // Note: call store.dispatch instead of next, so that the middleware order doesn't matter
-        store.dispatch(push(action.url))
+    if (action.url) {
+      // Note: call store.dispatch instead of next, so that the middleware order doesn't matter
+      switch(action.type) {
+        case redirectAction:
+          store.dispatch(push(action.url));
+          break;
+        case redirectAppendAction: {
+          // Not checking for undefined, assuming the router is always there
+          // Need to reconsider if any bug is reported
+          let currentBase = store.getState().router.location.pathname;
+          store.dispatch(push(`${currentBase}/${action.url}`));
+          break;
+        }
+        default:
+          break;
       }
     }
+
+    // Call next middleware even if the interested actions are caught,
+    // So that this middleware doesn't interrupt the users application in any way
     return next(action);
   }
 };
 
-export default createRedirectMiddleware("REDIRECT");
+export default createRedirectMiddleware("REDIRECT", "REDIRECT_APPEND");
